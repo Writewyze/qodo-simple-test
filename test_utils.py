f394649ccf69dc0f10c3b195a2a5c3015504d92c
twain <twain@192.168.1.56>
2026-01-29 13:14:00 +0530

    Initial commit: add user name utility

---
+++ test_utils.py
@@ -0,0 +1,5 @@
+from utils import get_user_name
+
+user = {"first": "John", "last": "Doe"}
+result = get_user_name(user)
+print(result)
---
+++ utils.py
@@ -0,0 +1,3 @@
+def get_user_name(user):
+    """Returns the user's full name."""
+    return f"{user['first']} {user['last']}"
